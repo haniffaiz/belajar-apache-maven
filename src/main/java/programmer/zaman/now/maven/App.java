86c99a90d51d52a01ac3b9550f281a005bbae153
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-05 04:18:09 +0700

    menambahkan dan menggunakan dependensi gson

--- src/main/java/programmer/zaman/now/maven/App.java
+++ src/main/java/programmer/zaman/now/maven/App.java
@@ -1,13 +1,19 @@
 package programmer.zaman.now.maven;
 
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+
+        Person person = new Person("Hanif Faiz Hidayat");
+        String json = gson.toJson(person);
+
+        System.out.println(json);
     }
 }
